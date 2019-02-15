84ef21759ccc3480f9ceae39da07f0897caef1dc
jboafo <jboafo@hotmail.co.uk>
2019-02-15 16:13:36 +0000

    unit test

--- src/test/java/io/java/springboot/topic/TopicTests.java
+++ src/test/java/io/java/springboot/topic/TopicTests.java
@@ -1,10 +1,16 @@
 package io.java.springboot.topic;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TopicTests {
 
     @Test
     public void fooTest() {
-
+        Topic topic1 = new Topic("1000","sue","girl");
+        Assert.assertEquals("1000", topic1.getId());
+        Assert.assertEquals("sue", topic1.getName());
+        Assert.assertEquals("girl", topic1.getDescription());
 
     }
 
