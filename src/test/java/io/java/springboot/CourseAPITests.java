88a3a8254d81ac96fef3b87b08de09d0122fd52f
edward ntim <edward.ntim@capgemini.com>
2019-02-15 17:17:35 +0000

    new changes

--- src/test/java/io/java/springboot/CourseAPITests.java
+++ src/test/java/io/java/springboot/CourseAPITests.java
@@ -1,11 +1,23 @@
 package io.java.springboot;
 
+import io.java.springboot.topic.TopicController;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CourseAPITests {
 
-    @Test
-    public void fooTest() {
+    @Autowired
+    TopicController controller;
 
+    @Test
+    //add and get tests
+    public void fooTest() {
+        controller.getAllTopics();
     }
+
 }
